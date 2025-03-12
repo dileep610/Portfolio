cf5fe106e80f413877085d81bff31f446d044d63
Dileep Chowdary <147240512+dileep610@users.noreply.github.com>
2025-03-12 21:26:01 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,3 @@
+// Placeholder for future JavaScript functionality (such as interactivity)
+// Currently, the page is static, but this file is set up for any dynamic changes in the future.
+console.log("Portfolio page loaded");
